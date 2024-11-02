857e21bb6fa3a924e969aa135dd0091809503c7c
Neeraj Bhatt <neerajwdev@gmail.com>
2024-11-02 23:41:39 +0530

    Ts-init

---
+++ Lesson01/build/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let username = "Neeraj bhatt";
+console.log(username);
+// typescript is a statically typed language , which means that the type of variable is known at compile time and cannot be changed at runtime. Typescript can infer the type of variable from the value assigned to it. 
+let a = 12;
+let b = "6";
+let c = 2;
+console.log(a / b);
+console.log(a * b);
---
+++ Lesson01/src/main.ts
@@ -0,0 +1,11 @@
+let username = "Neeraj bhatt"
+console.log(username);
+
+
+// typescript is a statically typed language , which means that the type of variable is known at compile time and cannot be changed at runtime. Typescript can infer the type of variable from the value assigned to it. 
+let a: number = 12;
+let b  = "6";
+let c: number = 2;
+
+console.log(a / b);
+console.log(a * b);
