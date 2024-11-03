3088e1d60df5008b6d7ee164e7d05918df866240
Neeraj Bhatt <neerajwdev@gmail.com>
2024-11-03 10:53:16 +0530

    basic-types

--- build/basic-types.js
+++ build/basic-types.js
@@ -2,3 +2,22 @@
 let num1 = "22";
 let num2 = 10;
 console.log(num1 / num2);
+// ++++ some data types ++++
+// cannot assign number to string
+let userName = "neeraj";
+userName = "Sumit"; // 22
+console.log(userName);
+let age;
+let isLoggedIn;
+let album;
+let album2;
+age = 21; // can be any number not any any other data type
+isLoggedIn = true; // can be true or false not any other data type
+album = "hello"; // can be any type
+album2 = "Raghav"; // can be number and string as it has a union type
+function addition(a, b) {
+    return a + b;
+}
+// union type is not limited to two data types
+let isActive;
+let reg = /\w+/g;
--- src/basic-types.ts
+++ src/basic-types.ts
@@ -1,3 +1,25 @@
 let num1 = "22"
 let num2 = 10
 console.log( num1 / num2);
+
+// ++++ some data types ++++
+// cannot assign number to string
+let userName:string = "neeraj"
+userName = "Sumit"  // 22
+console.log(userName);
+let age : number;
+let isLoggedIn : boolean;
+let album : any;
+let album2 : string | number;
+age = 21 // can be any number not any any other data type
+isLoggedIn = true   // can be true or false not any other data type
+album = "hello" // can be any type
+album2 = "Raghav" // can be number and string as it has a union type
+function addition (a: number,b: string) {
+    return a + b;
+}
+
+
+// union type is not limited to two data types
+let isActive :  boolean | number | string;
+let reg:RegExp = /\w+/g
